85d4eb700521ef6b225c6f00452520d62e8bde76
ArmanRey <checkoutarmn@gmail.com>
2020-04-29 14:35:19 +0530

    Test Upload 3

--- src/main/java/com/mobiquel/lms/LmsApplication.java
+++ src/main/java/com/mobiquel/lms/LmsApplication.java
@@ -2,8 +2,14 @@ package com.mobiquel.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.mobiquel.lms.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class LmsApplication {
 
 	public static void main(String[] args) {
